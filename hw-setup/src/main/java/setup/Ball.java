c11080bdac06cfe31987f40b5d76bed3bbe8d3e1
liujerry57 <liu.jerry57@gmail.com>
2021-01-19 20:02:08 -0800

    Implemented all methods in Box class for Part 7.3

--- hw-setup/src/main/java/setup/Ball.java
+++ hw-setup/src/main/java/setup/Ball.java
@@ -11,6 +11,8 @@
 
 package setup;
 
+import java.util.Comparator;
+
 /**
  * This is a simple object that has a volume.
  */
@@ -39,5 +41,26 @@ public class Ball {
     public double getVolume() {
         return volume;
     }
-
 }
+
+/**
+ * Object used to compare balls based on volume
+ */
+class SortByVolume implements Comparator<Ball> {
+
+    /**
+     * Compares balls for order based on volume
+     * @param b1 - first ball to be compared
+     * @param b2 - second ball to be compared
+     * @return a negative integer, zero, or a positive integer as the first
+     * argument is less than, equal to, or greater than the second
+     */
+    public int compare(Ball b1, Ball b2) {
+        return (int) (b1.getVolume() - b2.getVolume());
+    }
+}
+
+
+
+
+
--- hw-setup/src/main/java/setup/Box.java
+++ hw-setup/src/main/java/setup/Box.java
@@ -12,7 +12,7 @@
 package setup;
 
 import java.lang.Iterable;
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * This is a container can be used to contain Balls. The key
@@ -25,6 +25,7 @@ public class Box implements Iterable<Ball> {
      * ballContainer is used to internally store balls for this Box
      */
     private BallContainer ballContainer;
+    private double maxVolume;
 
     /**
      * Constructor that creates a new box.
@@ -32,8 +33,8 @@ public class Box implements Iterable<Ball> {
      * @param maxVolume Total volume of balls that this box can contain.
      */
     public Box(double maxVolume) {
-        // Your code goes here.  Remove the exception after you're done.
-        throw new RuntimeException("Method not implemented");
+        this.ballContainer = new BallContainer();
+        this.maxVolume = maxVolume;
     }
 
     /**
@@ -64,8 +65,10 @@ public class Box implements Iterable<Ball> {
      * @spec.requires b != null.
      */
     public boolean add(Ball b) {
-        // Your code goes here.  Remove the exception after you're done.
-        throw new RuntimeException("Method not implemented");
+        if (ballContainer.getVolume() + b.getVolume() <= maxVolume) {
+            return ballContainer.add(b);
+        }
+        return false;
     }
 
     /**
@@ -77,8 +80,11 @@ public class Box implements Iterable<Ball> {
      * ascending size.
      */
     public Iterator<Ball> getBallsFromSmallest() {
-        // Your code goes here.  Remove the exception after you're done.
-        throw new RuntimeException("Method not implemented");
+        List<Ball> tempContents = new ArrayList<>();
+        Iterator<Ball> itr= ballContainer.iterator();
+        itr.forEachRemaining(tempContents::add);
+        Collections.sort(tempContents, new SortByVolume());
+        return tempContents.iterator();
     }
 
     /**
