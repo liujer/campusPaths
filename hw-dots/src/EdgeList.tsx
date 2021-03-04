2d15cefdee1d7b327d3ce60632d18c4af1bdef3f
liujerry57 <liu.jerry57@gmail.com>
2021-03-04 07:03:25 -0800

    Improved error handling of edgeList in HW 8

--- hw-dots/src/EdgeList.tsx
+++ hw-dots/src/EdgeList.tsx
@@ -36,9 +36,13 @@ class EdgeList extends Component<EdgeListProps, any> {
 
     parseData = () : [[number, number], [number, number], string][] => {
         const edgesArr = this.state.edges.split('\n');
+        // regex searches for two coordinate pairs and a string
         const regex = /-?\d+,-?\d+\s+-?\d+,-?\d+\s+(.)*/;
+
         let result: [[number, number], [number, number], string][] = [];
         let lineNumber = 1;
+        let alertMsg = ""; // track alerts
+
         for (let edge of edgesArr) {
             if (edge.match(regex)) {
                 // Convert one line of data into array
@@ -51,32 +55,41 @@ class EdgeList extends Component<EdgeListProps, any> {
                     temp[2]
                 ];
                 // Add array element if valid line of input
-                if (this.validateInput(newEdge, lineNumber, this.props.size)) {
+                let testInput = this.validateInput(newEdge, lineNumber, this.props.size);
+                if (testInput == "") { // no errors from validation
                     result.push(newEdge);
+                } else {
+                    alertMsg = alertMsg.concat(testInput);
                 }
-            } else {
-                alert("Line " + lineNumber + ": Missing portion of line, or missing space");
+            } else if (edge != "") { // skip empty lines, just incorrect output
+                alertMsg = alertMsg.concat("Line " + lineNumber + ": Missing portion " +
+                    "of line, or missing space\n");
             }
             lineNumber++;
+        }
+        if (alertMsg != "") { // errors happened
+            alert(alertMsg);
         }
         return result; // array of all parsed and valid lines
     };
 
     validateInput = (edge : [[number, number], [number, number], string],
-                     lineNumber: number, gridSize: number) : boolean => {
+                     lineNumber: number, gridSize: number) : string => {
         let lineString = "Line " + lineNumber + ": ";
+        let alertMsg = ""; // track alerts
+        // iterate through all coordinates in edge
         for (let i = 0; i < edge.length - 1; i++) {
             for (let j = 0; j < edge.length - 1; j++) {
+                // add error warning if necessary
                 if (edge[i][j] < 0) {
-                    alert(lineString + "Point " + i + " has a negative coordinate");
-                    return false;
+                    alertMsg = alertMsg.concat(lineString + "Point " + i + " has a negative coordinate\n");
                 } else if (edge[i][j] >= gridSize) {
-                    alert(lineString + "Point " + i + " has a coordinate higher than grid size");
-                    return false;
+                    alertMsg = alertMsg.concat(lineString + "Point " + i + " has a coordinate " +
+                        "higher than grid size\n");
                 }
             }
         }
-        return true;
+        return alertMsg;
     };
 
     draw = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
